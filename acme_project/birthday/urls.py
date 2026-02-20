f118d31821ea2d9a4ba48215b09c173173d933c4
Aleksander Antipov <aleksant96@yandex.ru>
2026-02-20 17:48:10 +0500

    20feb26

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -6,4 +6,7 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
+    path('list/', views.birthday_list, name='list'),
+    path('<pk>/edit/', views.birthday, name='edit'),
+    path('<pk>/delete/', views.delete_birthday, name='delete'),
 ]
